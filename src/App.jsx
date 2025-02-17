9ec66d995b4273a2abc0699c9b2d2d596fe9d9d1
nikesh-trecarten <ntrecarten@gmail.com>
2025-02-17 18:31:05 +0100

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css'
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [input, setInput] = useState(""); // store user's input
@@ -10,17 +10,17 @@ function App() {
     console.log("Initial seq:", seq);
 
     while (number !== 1) {
-    if (number % 2 === 0) {
+      if (number % 2 === 0) {
         number = number / 2;
-    } else {
+      } else {
         number = number * 3 + 1;
-    };
-    seq.push(number); // add each step to the sequence array
+      }
+      seq.push(number); // add each step to the sequence array
     }
 
     console.log("Next Step:", number);
     seq.push(number);
-    
+
     console.log("Final sequence:", seq);
     return seq; // return the full sequence
   }
@@ -39,29 +39,40 @@ function App() {
 
   return (
     <>
-     <h1>The Collatz Sequence</h1>
-     <p>The Collatz Sequence is a famous unsolved problem in the study of mathematics. <br />
-     Take any number. If that number is even, divide it by 2. If it is odd, multiply it by 3 and add 1. <br />
-     Repeat this sequence over and over again. 
-     It will always eventually result in 1.</p>
-     <label htmlFor="input">Enter any number (except 1) to simulate the Collatz Sequence!</label>
-     <input type="number" id="input" value={input} onChange={(e) => setInput(e.target.value)} // update input state
-     />
-     <button onClick={handleSubmit}>Run Collatz Sequence</button>
-     <div>
-      {sequence.length > 0 && (
-        <div>
-          <h2>Sequence Steps:</h2>
-          <ul className='sequence-steps'>
-            {sequence.map((step, index) => (
-              <li key={index}>{step}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-     </div>
+      <h1 className="title">The Collatz Sequence</h1>
+      <p>
+        The Collatz Sequence is a famous unsolved problem in the study of
+        mathematics. <br />
+        Take any number. If that number is even, divide it by 2. If it is odd,
+        multiply it by 3 and add 1. <br />
+        Repeat this sequence over and over again. It will always eventually
+        result in 1.
+      </p>
+      <label htmlFor="input">
+        Enter any number (except 1) to simulate the Collatz Sequence!
+      </label>
+      <input
+        className="input"
+        type="number"
+        id="input"
+        value={input}
+        onChange={(e) => setInput(e.target.value)} // update input state
+      />
+      <button onClick={handleSubmit}>Run Collatz Sequence</button>
+      <div>
+        {sequence.length > 0 && (
+          <div>
+            <h2>Sequence Steps:</h2>
+            <ul className="sequence-steps">
+              {sequence.map((step, index) => (
+                <li key={index}>{step}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </>
   );
-  }
+}
 
-export default App
+export default App;
